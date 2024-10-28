745f6904487d73f08ee56d2262f0fa60f98e30af
Lautaro <lausuarez71@gmail.com>
2024-10-28 10:05:31 -0300

    fix: to address

--- components/Pay/index.tsx
+++ components/Pay/index.tsx
@@ -20,7 +20,7 @@ const sendPayment = async () => {
 
   const payload: PayCommandInput = {
     reference: id,
-    to: "0x0c892815f0B058E69987920A23FBb33c834289cf", // Test address
+    to: "0x52eF0e850337ecEC348C41919862dBAac42F620B", // Test address
     tokens: [
       {
         symbol: Tokens.WLD,
